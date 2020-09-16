d9abb464cc6c225b4baf007190a3a5be5d377830
LuizAlexandreSilva <alexandrehsilva96@gmail.com>
2020-09-15 20:31:15 -0400

    initial structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 
-import RocketseatLogo from '../assets/rocketseat.svg'
-
-import { Container } from '../styles/pages/Home'
+import { Container } from 'react-bootstrap'
 
 const Home: React.FC = () => {
   return (
@@ -12,7 +10,6 @@ const Home: React.FC = () => {
         <title>Homepage</title>
       </Head>
 
-      <RocketseatLogo />
       <h1>ReactJS Structure</h1>
       <p>A ReactJS + Next.js structure made by Rocketseat.</p>
     </Container>
